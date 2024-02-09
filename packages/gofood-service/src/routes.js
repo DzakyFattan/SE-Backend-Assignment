5c38cd856afb04027a368391bda45487cef84642
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2024-02-09 10:58:00 +0700

    fix: routing

--- packages/gofood-service/src/routes.js
+++ packages/gofood-service/src/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const { getOrder, requestFood } = require("./controller");
-const { authorizeUser } = require("./auth");
+const authorizeUser = require("./auth");
 
 const router = Router();
 
